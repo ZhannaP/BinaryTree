120a3dd6c7ffb4146e4f9be86ad15a6ebee7bbd3
ZhannaP <petkanichzhanna@gmail.com>
2023-09-24 16:46:37 +0300

    Node class added

--- Program.cs
+++ Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
+
 namespace BinaryTree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
+    }
+
+    public class Node<T> where T : IComparable
+    {
+        public T Data { get; set; }
+
+        public Node<T> ParentNode { get; set; }
+        public Node<T> LeftNode { get; set; }
+        public Node<T> RightNode { get; set; }
+        
+
+        public enum Side
+        {
+            Left,
+            Right
+        }
+
+        public Node(T data)
+        {
+            Data = data;
+        }
+
+
+        public override string ToString() => Data.ToString();
     }
 }
