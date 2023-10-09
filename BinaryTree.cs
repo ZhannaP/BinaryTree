9193841a12b84fe41f30e02f22831861e664f65e
ZhannaP <petkanichzhanna@gmail.com>
2023-10-09 21:40:48 +0300

    ver1

---
+++ BinaryTree.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinaryTree<T> where T : IComparable
+    {
+        // Корінь бінарного дерева
+        public Node<T> Root { get; set; }
+
+        // Додавання нового значення до бінарного дерева
+        public Node<T> Add(T data)
+        {
+            if (Root == null)
+            {
+                // Якщо дерево порожнє, створюємо новий корінь
+                Root = new Node<T>(data);
+                return Root;
+            }
+
+            Node<T> currentNode = Root;
+            while (true)
+            {
+                int comparisonResult = data.CompareTo(currentNode.Data);
+                if (comparisonResult == 0)
+                {
+                    return currentNode; // Значення вже існує у дереві
+                }
+                else if (comparisonResult < 0)
+                {
+                    if (currentNode.LeftNode == null)
+                    {
+                        // Додаємо новий вузол ліворуч
+                        currentNode.LeftNode = new Node<T>(data);
+                        currentNode.LeftNode.ParentNode = currentNode;
+                        return currentNode.LeftNode;
+                    }
+                    currentNode = currentNode.LeftNode;
+                }
+                else
+                {
+                    if (currentNode.RightNode == null)
+                    {
+                        // Додаємо новий вузол праворуч
+                        currentNode.RightNode = new Node<T>(data);
+                        currentNode.RightNode.ParentNode = currentNode;
+                        return currentNode.RightNode;
+                    }
+                    currentNode = currentNode.RightNode;
+                }
+            }
+        }
+
+        // Видалення вузла зі значенням `data` з дерева
+        public void Remove(T data)
+        {
+            Node<T> nodeToRemove = FindNode(data);
+            if (nodeToRemove != null)
+            {
+                Remove(nodeToRemove);
+            }
+        }
+
+        // Внутрішня функція для видалення вузла
+        private void Remove(Node<T> nodeToRemove)
+        {
+            if (nodeToRemove.LeftNode == null && nodeToRemove.RightNode == null)
+            {
+                // Case 1: Вузол не має нащадків (листок)
+                if (nodeToRemove.NodeSide == Node<T>.Side.Left)
+                {
+                    nodeToRemove.ParentNode.LeftNode = null;
+                }
+                else if (nodeToRemove.NodeSide == Node<T>.Side.Right)
+                {
+                    nodeToRemove.ParentNode.RightNode = null;
+                }
+                else
+                {
+                    // Вузол є коренем
+                    Root = null;
+                }
+            }
+            else if (nodeToRemove.LeftNode != null && nodeToRemove.RightNode != null)
+            {
+                // Case 3: Вузол має два нащадки
+                Node<T> successor = FindMin(nodeToRemove.RightNode);
+                nodeToRemove.Data = successor.Data;
+                Remove(successor);
+            }
+            else
+            {
+                // Case 2: Вузол має одного нащадка
+                Node<T> child = nodeToRemove.LeftNode ?? nodeToRemove.RightNode;
+                if (nodeToRemove.NodeSide == Node<T>.Side.Left)
+                {
+                    nodeToRemove.ParentNode.LeftNode = child;
+                }
+                else if (nodeToRemove.NodeSide == Node<T>.Side.Right)
+                {
+                    nodeToRemove.ParentNode.RightNode = child;
+                }
+                else
+                {
+                    // Вузол є коренем
+                    Root = child;
+                }
+                child.ParentNode = nodeToRemove.ParentNode;
+            }
+        }
+
+        // Пошук найменшого значення в піддереві, що починається з заданого вузла
+        private Node<T> FindMin(Node<T> node)
+        {
+            while (node.LeftNode != null)
+            {
+                node = node.LeftNode;
+            }
+            return node;
+        }
+
+        // Пошук вузла зі значенням в бінарному дереві
+        public Node<T> FindNode(T data)
+        {
+            Node<T> currentNode = Root;
+            while (currentNode != null)
+            {
+                int comparisonResult = data.CompareTo(currentNode.Data);
+                if (comparisonResult == 0)
+                {
+                    return currentNode; // Знайдено вузол із шуканим значенням
+                }
+                else if (comparisonResult < 0)
+                {
+                    currentNode = currentNode.LeftNode;
+                }
+                else
+                {
+                    currentNode = currentNode.RightNode;
+                }
+            }
+            return null; // Не знайдено в бінарному дереві
+        }
+
+        // Виведення бінарного дерева на екран
+        public void PrintTree()
+        {
+            PrintTree(Root, string.Empty);
+        }
+
+        // Внутрішня функція для виведення дерева
+        private void PrintTree(Node<T> node, string indent)
+        {
+            if (node != null)
+            {
+                Console.WriteLine($"{indent}{node.Data}");
+                if (node.LeftNode != null || node.RightNode != null)
+                {
+                    PrintTree(node.LeftNode, indent + "  |");
+                    PrintTree(node.RightNode, indent + "   ");
+                }
+            }
+        }
+
+        // Префіксний обхід бінарного дерева
+        public void PreOrderTraversal(Action<T> action)
+        {
+            PreOrderTraversal(Root, action);
+        }
+
+        // Внутрішня функція для префіксного обходу
+        private void PreOrderTraversal(Node<T> node, Action<T> action)
+        {
+            if (node != null)
+            {
+                action(node.Data);
+                PreOrderTraversal(node.LeftNode, action);
+                PreOrderTraversal(node.RightNode, action);
+            }
+        }
+
+        public void InOrderTraversal(Action<T> action)
+        {
+            InOrderTraversal(Root, action);
+        }
+
+        private void InOrderTraversal(Node<T> node, Action<T> action)
+        {
+            if (node != null)
+            {
+                InOrderTraversal(node.LeftNode, action);
+                action(node.Data);
+                InOrderTraversal(node.RightNode, action);
+            }
+        }
+
+        public void PostOrderTraversal(Action<T> action)
+        {
+            PostOrderTraversal(Root, action);
+        }
+
+        private void PostOrderTraversal(Node<T> node, Action<T> action)
+        {
+            if (node != null)
+            {
+                PostOrderTraversal(node.LeftNode, action);
+                PostOrderTraversal(node.RightNode, action);
+                action(node.Data);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,81 @@
 ﻿using System;
 
-
 namespace BinaryTree
 {
     class Program
     {
         static void Main(string[] args)
         {
-            
+            // Створення бінарного дерева типу int
+            var binaryTree = new BinaryTree<int>();
+
+            // Додавання значень у бінарне дерево
+            binaryTree.Add(5);
+            binaryTree.Add(2);
+            binaryTree.Add(11);
+            binaryTree.Add(14);
+            binaryTree.Add(9);
+            binaryTree.Add(0);
+            binaryTree.Add(1);
+            binaryTree.Add(12);
+            binaryTree.Add(20);
+
+            // Виведення бінарного дерева на екран
+            Console.WriteLine("Binary Tree:");
+            binaryTree.PrintTree();
+
+            // Роздільна лінія для відокремлення виводів
+            Console.WriteLine(new string('-', 40));
+
+            // Пошук вузла зі значенням 0
+            int valueToFind = 0;
+            Node<int> foundNode = binaryTree.FindNode(valueToFind);
+            if (foundNode != null)
+            {
+                Console.WriteLine($"Found {valueToFind}: {foundNode.Data}");
+            }
+            else
+            {
+                Console.WriteLine($"{valueToFind} not found in the tree.");
+            }
+
+            // Роздільна лінія
+            Console.WriteLine(new string('-', 40));
+
+            // Видалення вузла зі значенням 0
+            int valueToRemove = 9;
+            binaryTree.Remove(valueToRemove);
+            Console.WriteLine($"Removed {valueToRemove} from the tree.");
+
+            // Виведення оновленого бінарного дерева
+            Console.WriteLine("Updated Binary Tree:");
+            binaryTree.PrintTree();
+
+            // Очікування введення користувача перед закриттям програми
+            Console.ReadLine();
+
+            // Префіксний обхід і виведення значень дерева
+            Console.WriteLine("Pre-Order Traversal:");
+            binaryTree.PreOrderTraversal(data => Console.Write($"{data} "));
+            Console.WriteLine();
         }
     }
 
     public class Node<T> where T : IComparable
     {
+        // Значення даних, яке зберігається в вузлі
         public T Data { get; set; }
 
+        // Батьківський вузол
         public Node<T> ParentNode { get; set; }
-        public Node<T> LeftNode { get; set; }
-        public Node<T> RightNode { get; set; }
-        
 
+        // Вузол лівого дерева 
+        public Node<T> LeftNode { get; set; }
+
+        // Вузол правого дерева 
+        public Node<T> RightNode { get; set; }
+
+        // Перерахування, що визначає, чи вузол є лівим чи правим від батьківського вузла
         public enum Side
         {
             Left,
@@ -31,7 +87,28 @@ namespace BinaryTree
             Data = data;
         }
 
+        // Властивість NodeSide, яка визначає сторону (ліво чи право) вузла відносно батьківського вузла
+        public Side? NodeSide
+        {
+            get
+            {
+                if (ParentNode == null)
+                {
+                    return null; // Вузол кореневий 
+                }
+                else if (ParentNode.LeftNode == this)
+                {
+                    return Side.Left; // Вузол є лівим нащадком батьківського вузла
+                }
+                else
+                {
+                    return Side.Right; // Вузол є правим нащадком батьківського вузла
+                }
+            }
+        }
 
+        // Перевизначений метод ToString
         public override string ToString() => Data.ToString();
     }
+
 }
